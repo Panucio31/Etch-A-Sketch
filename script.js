4b3fa8c68515186e00197514715898a11921834e
Panucio31 <m.januska1990@gmail.com>
2022-09-05 21:36:18 +0300

    feat: :art: grid creation function done

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const gridContainer = document.querySelector('.grid-container');
 const button = document.querySelectorAll('.btn')
+const size = 0;
+
+const newGrid = (fieldCount) => {
+    gridContainer.style.gridTemplateColumns = `repeat(${fieldCount}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${fieldCount}, 1fr)`;
+
+    for (let i = 0; i < fieldCount * fieldCount; i++) {
+        const gridField = document.createElement('div');
+        gridField.classList.add('active-field');
+        gridField.addEventListener('mousedown', () => {
+            changeColor;
+        })
+        gridField.addEventListener('mouseover', () => {
+            changeColor
+        })
+        gridContainer.appendChild(gridField)
+    }
+}
+
+newGrid(64)
 
 
-const newGrid = () => {
-
+const changeColor = () => {
+    
 }
