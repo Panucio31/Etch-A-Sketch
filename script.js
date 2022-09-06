ddf0ccaaf47c11152611d120abf001112fe89acb
Panucio31 <m.januska1990@gmail.com>
2022-09-06 16:29:02 +0300

    feat: slider working

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const gridContainer = document.querySelector('.grid-container');
-const button = document.querySelectorAll('.btn')
-const size = 0;
+const button = document.querySelectorAll('.btn');
+const chooseColor = document.querySelector('#choose');
+const eraseField = document.querySelector('#erase');
+const clearAll = document.querySelector('#clear');
+const startingColor = document.querySelector('#color');
 
-const newGrid = (fieldCount) => {
-    gridContainer.style.gridTemplateColumns = `repeat(${fieldCount}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${fieldCount}, 1fr)`;
+// need default values for start
+const fieldCount = 16;
+const initialColor = 'gray';
+const defaultMode = 'color';
 
-    for (let i = 0; i < fieldCount * fieldCount; i++) {
+// mode select
+
+
+const newGrid = () => {
+    const input =  document.querySelector('.slider').value;
+    document.querySelector('.input').innerHTML = `Current size: ${input}`;
+    gridContainer.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${input}, 1fr)`;
+
+    for (let i = 0; i < input * input; i++) {
         const gridField = document.createElement('div');
         gridField.classList.add('active-field');
         gridField.addEventListener('mousedown', () => {
@@ -17,11 +30,22 @@ const newGrid = (fieldCount) => {
         })
         gridContainer.appendChild(gridField)
     }
+
+   
+
 }
 
-newGrid(64)
+newGrid()
 
 
-const changeColor = () => {
-    
+const changeColor = (e) => {
+    if (e.type === 'mousedown' && 'mouseover') {
+        e.target.style.backgroundColor = initialColor;
+
+    }
+
+    if (e.type === 'mousedown' && !'mouseover') {
+        return;
+    }
+
 }
