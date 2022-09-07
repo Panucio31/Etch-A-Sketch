18cf6b289dc22c3e7fa3245150189bc0f47ae481
Panucio31 <m.januska1990@gmail.com>
2022-09-07 20:42:44 +0300

    feat: color change function started

--- script.js
+++ script.js
@@ -1,56 +1,45 @@
-const gridContainer = document.querySelector('.grid-container');
-const button = document.querySelectorAll('.btn');
-const chooseColor = document.querySelector('#choose');
-const eraseField = document.querySelector('#erase');
-const clearAll = document.querySelector('#clear');
-const startingColor = document.querySelector('#color');
+const gridContainer = document.querySelector(".grid-container");
+const button = document.querySelectorAll(".btn");
+const chooseColor = document.querySelector("#choose");
+const eraseField = document.querySelector("#erase");
+const clearAll = document.querySelector("#clear");
+const startingColor = document.querySelector("#color");
 
 // need default values for start
 const fieldCount = 16;
-const initialColor = 'gray';
-const defaultMode = 'color';
+const initialColor = "white";
+const defaultMode = "color";
 
 // mode select
 
 //add grid
 
-
 const newGrid = () => {
-    const input =  document.querySelector('.slider').value;
-    document.querySelector('.input').innerHTML = `Current size: ${input}`;
-    gridContainer.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${input}, 1fr)`;
+  const input = document.querySelector(".slider").value;
+  document.querySelector(".input").innerHTML = `Current size: ${input}`;
+  gridContainer.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${input}, 1fr)`;
 
-    
+  gridContainer.innerHTML = "";
 
-    for (let i = 0; i < input * input; i++) {
-        const gridField = document.createElement('div');
-        gridField.classList.add('active-field');
-        gridField.addEventListener('mousedown', () => {
-            changeColor();
-        })
-        gridField.addEventListener('mouseover', () => {
-            changeColor();
-        })
-        gridContainer.appendChild(gridField);
-        console.log(input)
-        console.log(i)
-    }
+  for (let i = 0; i < input * input; i++) {
+    const gridField = document.createElement("div");
+    gridField.classList.add("active-field");
+    // gridField.addEventListener('mousedown', () => {
+    //     changeColor();
+    // })
+    gridField.addEventListener("mouseover", () => {
+      gridField.style.backgroundColor = "gray";
+      changeColor();
+    });
+    gridContainer.appendChild(gridField);
+  }
+};
 
-}
+newGrid();
 
-
-newGrid()
-
-
-const changeColor = (e) => {
-    // if (e.type === 'mousedown' && 'mouseover') {
-    //     e.target.style.backgroundColor = initialColor;
-
-    // }
-
-    // if (e.type === 'mousedown' && !'mouseover') {
-    //     return;
-    // }
-
-}
+const changeColor = () => {
+  const field = document.querySelector('active-field');
+  field.classList.add('erase');
+  field.style.backgroundColor = 'white';
+};
