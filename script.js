44a31d7c61552ae179341c8f248a74c6acf35013
Panucio31 <m.januska1990@gmail.com>
2022-09-12 18:52:25 +0300

    feat: :art: added ability to chose diferent color

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const chooseColor = document.querySelector("#choose");
 const eraseField = document.querySelector("#erase");
 const clearAll = document.querySelector("#clear");
 const startingColor = document.querySelector("#color");
+const colorPicker = document.querySelector('.choose');
+
 
 // need default values for start
 const fieldCount = 16;
-const initialColor = "white";
+const initialGridColor = "white";
+const initialDrawingColor = '#808080'
 const defaultMode = "color";
-
-// mode select
 
 //add grid
 
@@ -25,21 +26,42 @@ const newGrid = () => {
   for (let i = 0; i < input * input; i++) {
     const gridField = document.createElement("div");
     gridField.classList.add("active-field");
-    // gridField.addEventListener('mousedown', () => {
-    //     changeColor();
-    // })
     gridField.addEventListener("mouseover", () => {
-      gridField.style.backgroundColor = "gray";
-      changeColor();
+      gridField.style.backgroundColor = initialDrawingColor;
+      
     });
+    
     gridContainer.appendChild(gridField);
   }
+
+  shakeItOff();
+  
 };
+
+// delete grid container content
+const shakeItOff = () => {
+  eraseField.addEventListener('click', () => {
+    location.reload(gridContainer);
+  });
+}
+
+colorPicker.addEventListener('input', () => {
+  const input = document.querySelector(".slider").value;
+  console.log(input)
+  const newColor = document.querySelector('.choose').value;
+  console.log(newColor)
+  const gridField = gridContainer.children;
+  
+  for (let i = 0; i < input * input; i++) {
+    
+    gridField[i].addEventListener('mouseover', (e) => {
+      e.target.style.backgroundColor = newColor;
+      
+    })
+  }
+});
+
 
 newGrid();
 
-const changeColor = () => {
-  const field = document.querySelector('active-field');
-  field.classList.add('erase');
-  field.style.backgroundColor = 'white';
-};
+
